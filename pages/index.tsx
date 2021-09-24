28024eb03f607e6420f72088a0cea4278e58374d
roni.paschoal <roni.paschoal@ilog.com.br>
2021-09-24 09:03:35 -0300

    Corrigida a importação das fontes

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,9 +16,9 @@ const App: NextPage = () => {
         <title>{ data.head.title }</title>
         <link href="https://fonts.googleapis.com/css2?family=Merienda&display=swap" rel="stylesheet"></link>
         
-        <link href="../public/font/viking.ttf" rel="stylesheet"></link>
-        <link href="../public/font/futhark.otf" rel="stylesheet"></link>
-        <link href="../public/font/bastarda.ttf" rel="stylesheet"></link>
+        <link href="./font/viking.ttf" rel="preload" as="font" crossOrigin=""></link>
+        <link href="./font/futhark.otf" rel="preload" as="font" crossOrigin=""></link>
+        <link href="./font/bastarda.ttf" rel="preload" as="font" crossOrigin=""></link>
         
         <meta name="description" content={ data.head.description } />
         <link rel="icon" href="./images/favicon.png" type="image/png" />
