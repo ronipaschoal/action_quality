3ee73a7a4cba2f9eecf9a7ea3d6defb6654f3d23
roni.paschoal <roni.paschoal@ilog.com.br>
2021-10-01 22:28:59 -0300

    Criada função de envio de email

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,8 +1,9 @@
 import { NextPage } from 'next';
-import { useContext } from 'react';
+import { FormEvent, useContext, useState } from 'react';
 
 import styles from './styles.module.scss';
 import LanguageContext from '../../contexts/LanguageContext';
+import axios from 'axios';
 
 interface field {
     name: string,
@@ -23,11 +24,50 @@ interface Props {
 const Contact: NextPage<Props> = ({ data }) => {
 
   const language = useContext(LanguageContext);
+
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  })
+
+  const [status, setStatus] = useState('');
+
+  function handleChange(evt: any) {
+    const value = evt.target.value;
+    setState({
+      ...state,
+      [evt.target.name]: value
+    });
+  }
   
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    
+    var param = '';
+    Object.entries(state).map((value:Array<string>,index:number) => {
+      param += value[0] + '=' + value[1];
+      param += '&'
+    });
+
+    const API_PATH = `https://ronipaschoal.com.br/webservice/mail.php?${param.substring(0, param.length - 1)}`;    
+    const requestOptions = {
+        method: 'get',
+    };
+  
+    const response = await fetch(API_PATH, requestOptions).then(response =>{
+      return response.json();
+        }).then(data => {
+          console.log(data.success);
+    })
+  }
+
   return (
     <section id={data.id} className={styles.contact}>
-      <form action="">
+      <form onSubmit={ e => handleSubmit(e) }>
         <h2>{data.title[language.languageActive]}</h2>
+        <p>{status}</p>
         { data.fields.map((field, index) => {
             return(
               <div key={index}>
@@ -36,11 +76,13 @@ const Contact: NextPage<Props> = ({ data }) => {
                   <input type="text"
                   name={field.name}
                   id={field.name}
+                  onChange={handleChange}
                   placeholder={ field.placeholder[language.languageActive] } />
                 }
                 { field.type == "textarea" &&
                   <textarea name={field.name}
                   id={field.name}
+                  onChange={handleChange}
                   placeholder={ field.placeholder[language.languageActive] } />
                 }
               </div>
